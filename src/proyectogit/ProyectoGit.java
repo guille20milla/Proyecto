e277dddcbda3faaaa8727466c1b643d251110cf7
Alumno <Alumno@INF702>
2016-05-10 08:50:46 +0200

    Segundo commit

--- src/proyectogit/ProyectoGit.java
+++ src/proyectogit/ProyectoGit.java
@@ -15,7 +15,7 @@ public class ProyectoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Mi segundo guardado de commit");
     }
     
 }
